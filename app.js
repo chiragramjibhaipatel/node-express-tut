3533cb24ded2398f174cb64e19514e61ef8845d3
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2022-05-11 11:06:29 +0530

    read file using async function

--- app.js
+++ app.js
@@ -13,5 +13,16 @@ const getText = (path) => {
     })
 }
 
-getText('./content/first.txt').then(d => console.log(d)).catch(e=>console.log(e));
+const start = async () => {
+    try{
+        const file1 = await getText('./content/first.txt');
+        console.log(file1);
+    } catch (e){
+        throw e;
+    }
+}
+start().then(() => console.log("reading done")).catch((e) => console.log("some error", e));
 console.log("START")
+
+
+// getText('./content/first.txt').then(d => console.log(d)).catch(e=>console.log(e));
