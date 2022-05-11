d2eee83fb2d826ffc1f435dd86b1a84a89c5e6cb
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2022-05-11 10:26:05 +0530

    read file using Promise

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hey you");
+const {readFile} = require('fs')
+
+const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        readFile(path, 'utf8', (e, s) => {
+            if (e) {
+                reject(e);
+            } else {
+                resolve(s);
+            }
+        });
+
+    })
+}
+
+getText('./content/first.txt').then(d => console.log(d)).catch(e=>console.log(e));
+console.log("START")
