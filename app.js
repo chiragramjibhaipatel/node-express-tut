dd0b3b22203cbc692745967200ae7de75e49a9ce
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2022-05-12 10:24:53 +0530

    read file using read stream

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer(function (req, res){
-    const text = fs.readFileSync('./content/big.txt', 'utf-8');
-    res.end(text);
+
+    const fileReadStream = fs.createReadStream('./content/big.txt', { encoding: "utf-8"})
+    fileReadStream.on('open', data=>{
+        console.log("sending");
+        fileReadStream.pipe(res);
+    })
+    fileReadStream.on('error', e => {
+        res.end(e);
+    })
+
 }).listen(5000)
