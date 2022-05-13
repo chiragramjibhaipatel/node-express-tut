c6eb8e3297f2f61cc7d9c81ebb40a4eb8baeb3f7
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2022-05-13 11:19:21 +0530

    serve static content using express

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const path = require("path")
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, "./public")));
+
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./index.html"));
 })
 
+app.all("*", (req, res) => {
+    res.status(404).send("Not Found");
+});
+
 app.listen(5000, () => {
     console.log("server is listening on 5000");
-
 });
