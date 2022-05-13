0b05dd713e3a2b08c75aea9feb6e696f19a90f8d
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2022-05-13 22:46:18 +0530

    post using form

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const authorize = require('./authorize');
 
 app.use("/api/v1", [loggerV1, authorize]);
 app.use("/api/v2", loggerV2);
+app.use(express.urlencoded({extended: false}))
+app.use(express.static("./methods-public"))
+
+app.post("/login", (req, res) => {
+    console.log("asd", req.body);
+    res.send("hi");
+});
 
 app.get("/", (req, res) => {
     res.send("Home");
