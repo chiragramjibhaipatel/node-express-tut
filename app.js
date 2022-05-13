213c28339ac607b2f508179941357ee2ff41873a
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2022-05-13 13:13:05 +0530

    serve rest endpoint

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const express = require('express');
-const path = require("path")
+const {products} = require('./data');
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./index.html"));
-})
-
-app.all("*", (req, res) => {
-    res.status(404).send("Not Found");
+    res.json(products);
 });
 
-app.listen(5000, () => {
-    console.log("server is listening on 5000");
-});
+app.listen(5000);
