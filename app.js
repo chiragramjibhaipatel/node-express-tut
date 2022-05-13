c44b91d888fadbc91f0f19b390705c68b1b9bad2
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2022-05-13 17:14:05 +0530

    use req.param

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const {products} = require('./data');
 const app = express();
 
-
 app.get("/", (req, res) => {
-    res.json(products);
+    res.send("<h1>Hello</h1><a href='/get-products/1/55'>products</a>")
+
+});
+
+app.get("/get-products/:id/:something", (req, res) => {
+    console.log("id: ", req.params)
+    let productId = Number(req.params.id);
+    let newProducts = products.filter(p => p.id === productId);
+    res.json(newProducts);
 });
 
 app.listen(5000);
