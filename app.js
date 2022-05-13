785840ace82114b94ae001808c39ec74756812ef
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2022-05-13 18:36:51 +0530

    use req.query

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const express = require('express');
 const app = express();
 const {products} = require('./data');
 
-app.get("/product/:id", (req, res) => {
-    console.log("retrieve product");
-    let productId = Number(req.params.id);
-    if (productId) {
-        let product = products.filter(p => p.id === productId);
-        if (product.length !== 0) {
-            res.json(product);
-        } else {
-            res.send("product not found");
-        }
+app.get("/product", (req, res) => {
+    console.log(req.query);
+    let search = req.query.search;
+    let limit = req.query.limit;
+    let filteredProducts = [...products];
+    if (search) {
+        filteredProducts = filteredProducts.filter(p => p.name.startsWith(search));
+    }
+    if (limit) {
+        filteredProducts = filteredProducts.slice(0, Number(limit));
+    }
+    if (filteredProducts.length === 0) {
+        res.send("no match");
     } else {
-        res.send("invalid product number");
+        res.json(filteredProducts);
     }
 });
 
